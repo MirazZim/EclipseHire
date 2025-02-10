153ae686a319e9768c47d2f5b31cb15416fc1dfa
MirazZim <miraz.zim.38@gmail.com>
2025-02-10 13:58:00 +0600

    Did the navbar. Attention to detail. remember what i have did

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -2,12 +2,21 @@ import {
     createBrowserRouter,
     
   } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
 
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayout></MainLayout>,
+      errorElement: <h2>Route not found</h2>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>,
+        }
+      ]
     },
   ]);
 
