bf2fb296ca4d0ea3969fb67383434ac5caad8690
MirazZim <miraz.zim.38@gmail.com>
2025-02-10 15:27:46 +0600

    This is how we set authProvider or AuthContext. Remember it very well

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,12 @@ import {
 } from "react-router-dom";
 
 import router from './Router/router.jsx'
+import AuthProvider from './Context/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
